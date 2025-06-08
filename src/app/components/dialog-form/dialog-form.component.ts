ab6f87f69d75e44a1b42233229dc6404a0bfb95d
anggierz <anggie.rizo.zambrano@gmail.com>
2025-06-08 16:56:32 +0200

    add components

---
+++ src/app/components/dialog-form/dialog-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './dialog-form.component.html',
+  styleUrl: './dialog-form.component.scss'
+})
+export class DialogFormComponent {
+
+}
